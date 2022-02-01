0d6af98770dc47a68f3bcd69bdb25965e37bad17
Sanki0 <sebastian.ferreyra.c@uni.pe>
2022-01-31 22:14:52 -0500

    initial components & styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import FeedbackList from './components/FeedbackList';
+import Header from './components/Header';
+import FeedbackData from './data/FeedbackData';
 
 function App() {
+
+  const [feedback, setFeedback] = React.useState(FeedbackData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      <div className='container'>
+        <FeedbackList feedback={feedback} ></FeedbackList>
+      </div>
+
+    </>
   );
 }
 
