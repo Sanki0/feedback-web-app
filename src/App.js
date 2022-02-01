a367ac9b7cf9bb3e337c507f3cf8fd712073ccb8
Sanki0 <sebastian.ferreyra.c@uni.pe>
2022-02-01 16:34:10 -0500

    added stats & delete btn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
 import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
 
@@ -7,11 +8,22 @@ function App() {
 
   const [feedback, setFeedback] = React.useState(FeedbackData);
 
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure?')) {
+      setFeedback(feedback.filter((item) => (
+        item.id !== id
+      )))
+    }
+  }
   return (
     <>
       <Header></Header>
       <div className='container'>
-        <FeedbackList feedback={feedback} ></FeedbackList>
+        <FeedbackStats feedback={feedback}></FeedbackStats>
+        <FeedbackList
+          feedback={feedback}
+          handleDelete={deleteFeedback}
+        ></FeedbackList>
       </div>
 
     </>
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No feedback</p>
   }
   return (
     <div className='feedback-list'>
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} ></FeedbackItem>
+        <FeedbackItem
+          key={item.id}
+          item={item}
+          handleDelete={handleDelete}>
+        </FeedbackItem>
       ))}
     </div>
   )
