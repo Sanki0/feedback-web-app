9c786d3fba6117811a261c8192a38909dc9bd0e1
Sanki0 <sebastian.ferreyra.c@uni.pe>
2022-02-01 21:57:32 -0500

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { v4 as uuidv4 } from 'uuid';
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import FeedbackForm from './components/FeedbackForm';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
+import AboutPage from './pages/AboutPage';
+import AboutIconLink from './components/AboutIconLink';
 
 function App() {
 
@@ -22,18 +25,31 @@ function App() {
     setFeedback([newFeedback, ...feedback])
   }
   return (
-    <>
+    <BrowserRouter>
       <Header></Header>
       <div className='container'>
-        <FeedbackForm handleAdd={addFeedback}></FeedbackForm>
-        <FeedbackStats feedback={feedback}></FeedbackStats>
-        <FeedbackList
-          feedback={feedback}
-          handleDelete={deleteFeedback}
-        ></FeedbackList>
+        <Routes>
+
+
+          <Route exact path='/' element={
+            <>
+              <FeedbackForm handleAdd={addFeedback}></FeedbackForm>
+              <FeedbackStats feedback={feedback}></FeedbackStats>
+              <FeedbackList
+                feedback={feedback}
+                handleDelete={deleteFeedback}
+              ></FeedbackList>
+            </>
+          }>
+
+          </Route>
+
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink />
       </div>
 
-    </>
+    </BrowserRouter>
   );
 }
 
